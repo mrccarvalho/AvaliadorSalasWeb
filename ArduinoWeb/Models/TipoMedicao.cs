ac1b363204b99084648231fa8437c15b288e30fa
mrccarvalho <mrcc@outlook.pt>
2024-03-11 12:39:12 +0000

    Add project files.

---
+++ ArduinoWeb/Models/TipoMedicao.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Metrics;
+
+namespace ArduinoWeb.Models
+{
+    public class TipoMedicao
+    {
+        [Required]
+        public int TipoMedicaoId { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public virtual ICollection<Medicao> Medicoes { get; set; }
+    }
+
+    public enum TipoMedicaoEnum
+    {
+        Temperatura = 1,
+        Humidade = 2,
+        Co2 = 3
+    }
+}
---
+++ ArduinoWeb/ViewModels/MedicaoVm.cs
@@ -0,0 +1,22 @@
+﻿namespace ArduinoWeb.ViewModels
+{
+    public class MedicaoVm
+    {
+        public DateTime? DataMedicao { get; set; }
+        public string NomeLocalizacao { get; set; }
+        public decimal? Temperatura { get; set; }
+        public decimal? Humidade { get; set; }
+        public decimal? Co2 { get; set; }
+
+        public string DateOnlyString => DataMedicao?.ToString("yyyy-MM-dd") ?? string.Empty;
+        public string TimeOnlyString => DataMedicao?.ToString("hh:mm:ss tt") ?? string.Empty;
+
+        public string GoogleDate => (DataMedicao.HasValue)
+          ? string.Format("Date({0})", DataMedicao.Value.ToString("yyyy,M,d,H,m,s,f"))
+          : string.Empty;
+
+        public string TempString => (Temperatura != null) ? Temperatura.Value.ToString("###.0") : "0.0";
+        public string HumidString => (Humidade!= null) ? Humidade.Value.ToString("###.0") : "0.0";
+        public string Co2String => (Co2 != null) ? Co2.Value.ToString("###.0") : "0.0";
+    }0.
+}
